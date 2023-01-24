990383d9a97beffd6f272bba47b4b86e595e9cab
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2023-01-24 11:14:31 -0700

    worked_variable_args

--- app.py
+++ app.py
@@ -2,3 +2,29 @@ def print_order(*order_items):
     print(order_items)
 
 print_order("apple juice","orange juice","pancakes")
+
+
+tables={
+    1: {
+        "name": "satinder",
+        "vip_status": True,
+        "order": "apple juice, Orange juice"
+    },
+    2: {
+
+    },
+    3: {
+
+    },
+    4: {}
+}
+
+
+def assign_table(table_number, name, vip_status,*order_items):
+    tables[table_number]['name'] = name
+    tables[table_number]['vip_status'] = vip_status
+    tables[table_number]['order'] = order_items
+
+    print(tables)
+
+assign_table(2,"sam",True,"orange juice","apple juice")
